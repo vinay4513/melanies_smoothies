cce1eccc231bbdf66f9e1cfaa7540e6bad0ca50f
Vinay Satish Gadade <133940610+vinay4513@users.noreply.github.com>
2024-05-27 15:06:04 +0530

    Update Streamit_app.py

--- Streamit_app.py
+++ Streamit_app.py
@@ -15,7 +15,7 @@ st.write(
 )
 
 cnx = st.connection("snowflake")
-session = cnx.session
+session = cnx.session()
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
