1264081b635c7b2777f7d797869a8b96eb3141d9
Vinay Satish Gadade <133940610+vinay4513@users.noreply.github.com>
2024-05-28 13:43:19 +0530

    Update Streamit_app.py

--- Streamit_app.py
+++ Streamit_app.py
@@ -18,8 +18,9 @@ st.write(
 cnx = st.connection("snowflake")
 session = cnx.session()
 
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
+st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop()
 
 
 Ingredients_list = st.multiselect("choose upto 5 ingrediants :"
